b2de93ade265e695787eaee6241309f644e08a5a
Shoyoo01 <ja_diaf@esi.dz>
2023-12-15 22:57:05 +0100

    Second

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,10 +1,59 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Image.asset(
+                "assets/images/main_top.png",
+                width: 118,
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(33),
+                    child: const Text(
+                      "Login",
+                      style: TextStyle(fontSize: 26, fontFamily: "myfont"),
+                    ),
+                  ),
+                  SvgPicture.asset("assets/icons/login.svg"),
+                  Container(
+                    color: Colors.amber,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          labelText: "Email",
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.email),
+                          suffixIcon: Icon(Icons.password)),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Positioned(
+              width: 150,
+              bottom: 0,
+              right: 0,
+              child: Image.asset("assets/images/login_bottom.png"),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Welcome extends StatelessWidget {
   const Welcome({super.key});
@@ -10,14 +11,75 @@ class Welcome extends StatelessWidget {
         body: Stack(
           children: [
             Positioned(
-              child: Image.asset("assets/images/main_top.png"),
               top: 0,
               left: 0,
+              child: Image.asset(
+                "assets/images/main_top.png",
+                width: 118,
+              ),
             ),
             Positioned(
-              child: Image.asset("assets/images/main_bottom.png"),
+              child: SizedBox(
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.all(33),
+                      child: const Text(
+                        "Welcome Brozar",
+                        style: TextStyle(fontSize: 26, fontFamily: "myfont"),
+                      ),
+                    ),
+                    Container(
+                        margin: const EdgeInsets.fromLTRB(30, 0, 30, 18),
+                        child: SvgPicture.asset(
+                          "assets/icons/chat.svg",
+                        )),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/login");
+                      },
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.purple),
+                        padding: MaterialStateProperty.all(
+                            const EdgeInsets.symmetric(horizontal: 79, vertical: 10)),
+                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(27))),
+                      ),
+                      child: const Text(
+                        "LOGIN",
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 22,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/signup");
+                      },
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.purple[100]),
+                        padding: MaterialStateProperty.all(
+                            const EdgeInsets.symmetric(horizontal: 77, vertical: 13)),
+                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(27))),
+                      ),
+                      child: Text(
+                        "SIGNUP",
+                        style: TextStyle(fontSize: 18, color: Colors.grey[850]),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
               bottom: 0,
               left: 0,
+              child: Image.asset("assets/images/main_bottom.png"),
             ),
           ],
         ),
